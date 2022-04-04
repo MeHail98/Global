4f106b951285c7c0d2c0fc8e81a575f8542c6923
MeHail98 <mehail.conversation@gmil.com>
2022-04-04 20:05:43 +0300

    creation of the project

---
+++ src/by/itstep/rodionov/javastages/stage12/task1/controller/Controller.java
@@ -0,0 +1,23 @@
+package by.itstep.rodionov.javastages.stage12.task1.controller;
+
+import by.itstep.rodionov.javastages.stage12.task1.view.Printer;
+
+import java.util.Scanner;
+
+public class Controller {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Printer.print("Input 4 numbers:");
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        boolean result = true;
+
+        String msg = result? "Yes": "No";
+
+        Printer.print(msg);
+
+    }
+}
---
+++ src/by/itstep/rodionov/javastages/stage12/task1/logic/ChessLogic.java
@@ -0,0 +1,7 @@
+package by.itstep.rodionov.javastages.stage12.task1.logic;
+
+public class ChessLogic {
+    public static boolean chekRookStep(int x1,int x2,int y1,int y2) {
+        return false;
+    }
+}
